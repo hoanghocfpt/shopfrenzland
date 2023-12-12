e47e24fca7590f73fec9addb54792d10c15e7441
Phạm Văn Hoàng <iloveanh18@gmail.com>
2023-12-12 13:28:01 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Jost } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/layout/Header'
 import Footer from '@/components/layout/Footer'
+import { CartProvider } from '@/context/CartContext'
 
 
 const jost= Jost({ weight: ['100','200','300','400','500','600','700','800','900'],subsets: ['latin'] })
@@ -15,13 +16,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jost.className}>
-      <div className=''>
-        <Header/>
-        <main className=''>
-          {children}
-        </main>
-        <Footer/>
-      </div>
+        <div className=''>
+          <CartProvider>
+            <div className="" >
+              <Header />
+              <main className="">
+                {children}
+              </main>
+              <Footer />
+            </div>
+          </CartProvider>
+        </div>
       
       </body>
     </html>
---
+++ src/context/CartContext.js
@@ -0,0 +1,33 @@
+"use client";
+import { createContext, useState, useEffect } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem('cartItem'));
+    if (Array.isArray(storedCart)) {
+      setCart(storedCart);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cartItem', JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCart(cart.filter((item) => item.id !== itemId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
