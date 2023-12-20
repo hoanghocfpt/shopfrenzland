eb80b55486715c3c90025f033294a93068293f04
Phạm Văn Hoàng <iloveanh18@gmail.com>
2023-12-21 01:45:58 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import HomePage from './home/page'
+import ProductDEMO from './demo/page'
+import PreviewPage from './demo/page'
 
 export default function Home() {
   return (
     <>
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <PreviewPage></PreviewPage>
     </>
   )
 }
---
+++ src/app/success/success.js
@@ -0,0 +1,9 @@
+// pages/success.js
+export default function SuccessPage() {
+    return (
+      <div>
+        <h1>Thanh toán thành công!</h1>
+        <p>Cảm ơn bạn đã mua hàng.</p>
+      </div>
+    );
+  }
